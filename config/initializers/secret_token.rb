3be66b70f06850b9d735f7906eda4775fba120ea
Eric Haase <erichaase@gmail.com>
2013-09-20 15:00:15 -0500

    Added default rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fantasy::Application.config.secret_key_base = '9799d8f4c022acb418ba7463d2f820b45c109833247b229ae69ebceb2d918c3a9e9d1a217f639a810d14c8f80a4b87bf5bb8a9ee1e1a24912ab178d10d5523f7'
